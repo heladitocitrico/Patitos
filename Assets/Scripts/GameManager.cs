202f8227e166083e49bb349b9e1edaef032733e0
Marcelo <marcelomendez6240@gmail.com>
2024-01-27 20:15:30 -0300

    Select random character and set sprite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.PSD;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,9 +12,17 @@ public class GameManager : MonoBehaviour
     
     public ChistesData chistesData;
     public TextMeshProUGUI chisteText;
+    public Player.Pueblo PuebloID;
+
+    public GameObject CampesinoObject;
+    public GameObject CleroObject;
+    public GameObject NobleObject;
+    public GameObject BufonObject;
 
     void Start()
     {
+        SelectRadomharacter();
+
         StartCoroutine(EntrarPersonaje());
     }
 
@@ -27,5 +36,42 @@ public class GameManager : MonoBehaviour
         System.Random random = new System.Random();
         var chiste = chistesData.chistes[random.Next(0, chistesData.chistes.Length)];
         chisteText.text = chiste.texto;
+    }
+
+    void SelectRadomharacter()
+    {
+        System.Random r =  new System.Random();
+        PuebloID = (Player.Pueblo)r.Next(0, 3);
+
+    DesactivateSprites();
+        switch (PuebloID)
+        {
+            case Player.Pueblo.Campesino :
+            CampesinoObject.SetActive(true);
+            break;
+
+            case Player.Pueblo.Clerigo :
+            CleroObject.SetActive(true);
+            break;
+
+            case Player.Pueblo.Noble :
+            NobleObject.SetActive(true);
+            break;
+
+            case Player.Pueblo.Bufon :
+            BufonObject.SetActive(true);
+            break;
+
+            default:
+            break;
+        }
+    }
+
+    void DesactivateSprites()
+    {
+        CampesinoObject.SetActive(false);
+        CleroObject.SetActive(false);
+        NobleObject.SetActive(false);
+        BufonObject.SetActive(false);
     }
 }
