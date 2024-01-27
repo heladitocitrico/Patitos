fd6fd32ec511d87942c8b73e00e757f349d2da67
Marcelo <marcelomendez6240@gmail.com>
2024-01-27 16:43:18 -0300

    Delete gamemode and modify player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,43 +6,25 @@ using Unity.VisualScripting;
 
 public class Player : MonoBehaviour
 {
-    public Slider CampesinoSlide;
-    public Slider ClerigoSlide;
-    public Slider ReySlide;
-    public Slider BuffonesSlide;
-
-    public float CampesinosPercent = 0.5f;
-    public float ClerigoPercent = 0.5f;
-    public float ReyesPercent = 0.5f;
-    public float BuffonesPercent = 0.5f;
-
-    void Update()
-    {
-        if (CampesinoSlide)
-        {
-<<<<<<< Updated upstream
-            CampesinoSlide.value = CampesinosPercent;
-            ReySlide.value = ReyesPercent;
-            ClerigoSlide.value = ClerigoPercent;
-            BuffonesSlide.value = BuffonesPercent;
-        }
-    }
-=======
-            SliderCampesino.fillAmount = CampesinoPercent / 100;
-            SliderClerigo.fillAmount = ClerigoPercent / 100;
-            SliderNobles.fillAmount = NoblesPercent / 100;
-            SliderBufones.fillAmount = BufonPercent / 100;
-        }
-    }
-
-    public Image SliderCampesino;
-    public Image SliderClerigo;
-    public Image SliderNobles;
-    public Image SliderBufones;
+    public Image CampesinoSlider;
+    public Image ClerigoSlider;
+    public Image NoblesSlider;
+    public Image BufonesSlider;
 
     public float CampesinoPercent;
     public float ClerigoPercent;
     public float NoblesPercent;
     public float BufonPercent;
->>>>>>> Stashed changes
+
+    void Update()
+    {
+
+        if (CampesinoSlider)
+        {
+            CampesinoSlider.fillAmount = CampesinoPercent / 100;
+            ClerigoSlider.fillAmount = ClerigoPercent / 100;
+            NoblesSlider.fillAmount = NoblesPercent / 100;
+            BufonesSlider.fillAmount = BufonPercent / 100;
+        }
+    }
 }
