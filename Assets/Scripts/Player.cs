4bc221dcf9e977a39a9e7f9b56f2cfe854d334c3
Marcelo <marcelomendez6240@gmail.com>
2024-01-27 17:40:20 -0300

    Game over anim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,25 +6,74 @@ using Unity.VisualScripting;
 
 public class Player : MonoBehaviour
 {
+    public enum Pueblo
+    {
+        Campesino = 0,
+        Clerigo,
+        Noble,
+        Bufon,
+    }
+
+    public Animator GameOverAnim;
+
     public Image CampesinoSlider;
     public Image ClerigoSlider;
     public Image NoblesSlider;
     public Image BufonesSlider;
 
-    public float CampesinoPercent;
-    public float ClerigoPercent;
-    public float NoblesPercent;
-    public float BufonPercent;
+    public int CampesinoPercent;
+    public int ClerigoPercent;
+    public int NoblesPercent;
+    public int BufonPercent;
 
-    void Update()
+    void Start()
     {
+        SetFillAmount(Pueblo.Campesino);
+        SetFillAmount(Pueblo.Clerigo);
+        SetFillAmount(Pueblo.Noble);
+        SetFillAmount(Pueblo.Bufon);
 
-        if (CampesinoSlider)
+    }
+
+    public void SetFillAmount(Pueblo Type, int Value = 0)
+    {
+        switch (Type)
         {
-            CampesinoSlider.fillAmount = CampesinoPercent / 100;
-            ClerigoSlider.fillAmount = ClerigoPercent / 100;
-            NoblesSlider.fillAmount = NoblesPercent / 100;
-            BufonesSlider.fillAmount = BufonPercent / 100;
+            case Pueblo.Campesino:
+                CampesinoPercent += Value;
+                CampesinoSlider.fillAmount = CampesinoPercent / 100.0f;
+
+                if (CampesinoPercent <= 0) GameOver();
+
+                break;
+
+            case Pueblo.Clerigo:
+                ClerigoPercent += Value;
+                ClerigoSlider.fillAmount = ClerigoPercent / 100.0f;
+                if (ClerigoPercent <= 0) GameOver();
+                break;
+
+            case Pueblo.Noble:
+                NoblesPercent += Value;
+                NoblesSlider.fillAmount = NoblesPercent / 100.0f;
+                if (NoblesPercent <= 0) GameOver();
+
+                break;
+
+            case Pueblo.Bufon:
+                BufonPercent += Value;
+                BufonesSlider.fillAmount = BufonPercent / 100.0f;
+                if (BufonPercent <= 0) GameOver();
+                break;
+
+            default:
+                Debug.Log("Type undifinded");
+                break;
         }
+    }
+
+    void GameOver()
+    {
+        GameOverAnim.SetTrigger("GameOver");
     }
 }
